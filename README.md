6530a239f8172ac4594bff80ba358661eee762f6
Tvax <19556689+Tvax@users.noreply.github.com>
2017-08-31 18:11:48 +0200

    script upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# acServer-install
-Easy install script to install an Assetto Corsa server on Ubuntu
+# ASETTO CORSA INSTALL SCRIPT
+Easy install script to install an Assetto Corsa server on Ubuntu !
+
+## Install  
+`wget https://raw.githubusercontent.com/Tvax/acServer-install/master/acServer-install.sh;`  
+`chmod 755 acServer-install.sh;`  
+`./acServer-install.sh;`  
---
+++ acServer-install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+steamcmd(){
+	sudo apt-get install lib32gcc1;
+	sudo apt-get install curl;
+	mkdir ~/Steam && cd ~/Steam;
+	curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+}
+
+acDownload(){
+	echo "Steam need to check if you own the game before downloading the files.";
+	echo "Neither of your username and password will be saved !";
+	echo "Enter your Steam username : ";
+	read username;
+	echo "Enter your Steam password: ";
+	read password;
+
+	read -e -p "Path to download Assetto Corsa's server files" -i "~/AC" PATH
+	mkdir $PATH;
+	./steamcmd.sh +@sSteamCmdForcePlatformType windows +login $username $password +force_install_dir $PATH +app_update 302550 +quit
+}
+
+##Check if system compatible before install
+compatible(){
+	##Check if distribution is supported
+	if [[ -z "$(uname -a | grep Ubuntu)" && -z "$(uname -a | grep Debian)" ]];then
+		return 1
+	fi
+}
+
+##Updates && Upgrades
+updates(){
+	sudo apt-get update;
+	sudo apt-get upgrade;
+}
+
+main(){
+	##call compatible to check if distro is either Debian or Ubuntu
+	compatible
+	if [ $? == 1 ]; then
+		echo Distro not supported
+		exit 1
+	fi
+	##call updates to upgrade the system
+	updates
+	steamcmd
+	acDownload
+}
+
+main
+
+echo "Successfully downloaded !";
