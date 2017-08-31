e86648aa75ff0fd3858f56713f0d4146478bcced
Tvax <19556689+Tvax@users.noreply.github.com>
2017-08-31 18:55:19 +0200

    directory

--- acServer-install.sh
+++ acServer-install.sh
@@ -15,9 +15,8 @@ acDownload(){
 	echo "Enter your Steam password: ";
 	read password;
 
-	read -e -p "Path to download Assetto Corsa's server files : " -i "~/AC" PATH;
-	echo $PATH;
-	mkdir $PATH;
+	read -e -p "Path to download Assetto Corsa's server files : " -i "$HOME/AC" directory;
+	mkdir -p "$directory";
 	./steamcmd.sh +@sSteamCmdForcePlatformType windows +login $username $password +force_install_dir $PATH +app_update 302550 +quit
 }
 
